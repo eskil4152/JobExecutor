dc23d585d488766f323533f8384795d14e767706
eskil <eskil@blikeng.com>
2026-03-23 23:50:26 +0100

    Added logging to JobExecutionService

--- src/main/java/com/blikeng/job/executor/JobExecutorApplication.java
+++ src/main/java/com/blikeng/job/executor/JobExecutorApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class Main {
+public class JobExecutorApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Main.class).run(args);
+        new SpringApplicationBuilder(JobExecutorApplication.class).run(args);
     }
 }
--- src/main/java/com/blikeng/job/executor/service/JobExecutionService.java
+++ src/main/java/com/blikeng/job/executor/service/JobExecutionService.java
@@ -4,6 +4,8 @@ import com.blikeng.job.executor.domain.JobEntity;
 import com.blikeng.job.executor.payloads.AddNumbersPayload;
 import com.blikeng.job.executor.payloads.CountWordsPayload;
 import com.blikeng.job.executor.repository.JobRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
 
@@ -13,6 +15,7 @@ import java.util.UUID;
 public class JobExecutionService {
     private final JobRepository jobRepository;
     private final ObjectMapper objectMapper;
+    private final Logger logger = LoggerFactory.getLogger(JobExecutionService.class);
 
     public JobExecutionService(JobRepository jobRepository, ObjectMapper objectMapper) {
         this.jobRepository = jobRepository;
@@ -20,12 +23,15 @@ public class JobExecutionService {
     }
 
     public void execute(UUID jobId) {
-        System.out.println("Started");
-
         sleep(5000);
 
         JobEntity job = jobRepository.findById(jobId)
-            .orElseThrow(() -> new IllegalArgumentException("Job not found"));
+            .orElseThrow(() -> {
+                logger.error("Job {} not found", jobId);
+                return new IllegalArgumentException("Job not found");
+            });
+
+        logger.info("Executing job {} of type {}", jobId, job.getJobType());
 
         job.markJobStarted();
         jobRepository.save(job);
@@ -36,32 +42,33 @@ public class JobExecutionService {
             switch (job.getJobType()) {
                 case ADD_NUMBERS -> result = handleAddNumbers(job);
                 case COUNT_WORDS -> result = handleCountWords(job);
-                default -> throw new IllegalArgumentException("Unsupported job type");
+                default -> {
+                    logger.error("Job type {} not supported", job.getJobType());
+                    throw new IllegalArgumentException("Job type not supported");
+                }
             }
 
             job.markJobFinished(result);
             jobRepository.save(job);
 
         } catch (Exception e) {
-            job.markJobFailed(e.getMessage());
+            job.markJobFailed("Job failed, please try again later.");
             jobRepository.save(job);
+
+            logger.error("Job {} of type {} failed", jobId, job.getJobType(), e);
         }
 
-        System.out.println("Finished");
+        logger.info("Finished job {} of type {}", jobId, job.getJobType());
     }
 
     private String handleAddNumbers(JobEntity job) {
         AddNumbersPayload payload = objectMapper.readValue(job.getPayload(), AddNumbersPayload.class);
-
-        sleep(5001);
 
         return String.valueOf(payload.a() + payload.b());
     }
 
     private String handleCountWords(JobEntity job) {
         CountWordsPayload payload = objectMapper.readValue(job.getPayload(), CountWordsPayload.class);
-
-        sleep(5002);
 
         return "OK";
     }
